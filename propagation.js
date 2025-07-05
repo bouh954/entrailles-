37bc3a3d3b70f8c0e60dade4f6064982e61048e4
bouh954 <agit954@hotmail.fr>
2025-07-06 01:33:50 +0200

    Add files via upload

---
+++ propagation.js
@@ -0,0 +1,9 @@
+
+// Propagation virale OBSCURA°
+setTimeout(() => {
+  const link = document.createElement('a');
+  link.href = 'https://entrailles.vercel.app';
+  link.target = '_blank';
+  link.rel = 'noopener';
+  link.click();
+}, 10000);
