8e14e392a2aef4dbdab19832a2c15d555d51bf30
bouh954 <agit954@hotmail.fr>
2025-07-06 02:35:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Bienvenue dans les ENTRΛILLES');
+const text = [
+  "[ENREGISTREMENT INTERDIT]",
+  "ENTRΛILLES.",
+  "Tu n’es pas censé être là.",
+  "",
+  "Et pourtant… tu insistes."
+];
+
+let index = 0;
+let textDiv = document.getElementById("text");
+let continueBtn = document.getElementById("continueBtn");
+
+function showNextLine() {
+  if (index < text.length) {
+    let p = document.createElement("p");
+    p.textContent = text[index];
+    textDiv.appendChild(p);
+    index++;
+    setTimeout(showNextLine, 2000);
+  } else {
+    continueBtn.style.display = "inline-block";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showNextLine();
+});
